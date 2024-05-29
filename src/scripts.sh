78f2e00ee5edf3179b7c918c183641b13d955950
Deepak Sapkota <deepak@portpro.io>
2024-05-29 11:04:55 +0545

    refact : console beautify

--- src/scripts.sh
+++ src/scripts.sh
@@ -13,6 +13,7 @@ fi
 # @example : checkFlutterVersion
 
 checkFlutterVersion() {
+    run_with_spinner "Checking Flutter version..." "Flutter version details:" printf ""
     flutter --version
 }
 
@@ -26,7 +27,7 @@ locallyInstalledFlutterVersionsList() {
 
     # Get branch list
     branch_list=$(git -C "$flutter_root" branch)
-
+    run_with_spinner "Fetching locally installed flutter versions..." "Available versions are: " printf ""
     # Print the branch list
     printf "%s\n" "$branch_list"
 
@@ -37,10 +38,9 @@ locallyInstalledFlutterVersionsList() {
 # @example : updateFSMVersion
 
 updateFSMVersion() {
-    printf "%b%s%b\n" "$BLUE" "Updating fsm..." "$NC"
-    rm -rf ~/.fsm
-    git clone https://github.com/sjdpk/fsm.git ~/.fsm
-    printf "%b%s%b\n" "$RED" "Update shell eg: [ source ~/.zshrc, source ~/.bashrc, ...]" "$NC"
+    command="rm -rf ~/.fsm && git clone https://github.com/sjdpk/fsm.git ~/.fsm >/dev/null 2>&1"
+    run_with_spinner "Updating fsm..." "fsm updated successfully" eval "$command"
+    printf "%b! %s%b\n" "$YELLOW" "Update shell eg: [ source ~/.zshrc, source ~/.bashrc, ...]" "$NC"
 }
 
 # @desc : switch flutter sdk version
@@ -51,11 +51,9 @@ updateFSMVersion() {
 flutterSDKVersionSwitch() {
     version_to_use=$1
     flutter_root=$(cd "$(dirname "$(which flutter)")/.." && pwd)
-    printf "%bSwitching Progress... %b\n" "$GREEN" "$NC"
 
-    # Suppress output from git checkout
-    git -C "$flutter_root" checkout "$version_to_use" >/dev/null 2>&1
-    printf "%b%s%b\n" "$YELLOW" "Switched to Flutter $version_to_use" "$NC"
+    command="git -C '$flutter_root' checkout '$version_to_use' >/dev/null 2>&1"
+    run_with_spinner "Processing..." "Switched to Flutter $version_to_use " eval "$command"
 }
 
 # @desc : check if branch is present
@@ -66,11 +64,12 @@ flutterSDKVersionSwitch() {
 isBranchPresent() {
     branch_to_check=$1
     flutter_root=$(cd "$(dirname "$(which flutter)")/.." && pwd)
+    run_with_spinner "Checking Flutter version $branch_to_check" "" printf ""
     check=$(git -C "$flutter_root" branch | grep "$branch_to_check")
     if [ -z "$check" ]; then
         return 0
     else
-        printf "%bFlutter version %s present in your local system.%b\n" "$BLUE" "$branch_to_check" "$NC"
+        logError "Flutter version $branch_to_check is already present in your local system."
         printf "%bHINT: Please switch version%b\n" "$YELLOW" "$NC"
         exit 1
     fi
@@ -81,18 +80,14 @@ isBranchPresent() {
 # @param : $1 - version_to_install
 # @return : download flutter version
 # @example : downloadFlutterVersion "stable"
-
 downloadFlutterVersion() {
     version_to_install=$1
     isBranchPresent "$version_to_install"
     current_branch=$(git -C "$flutter_root" branch --show-current)
     flutter_root=$(cd "$(dirname "$(which flutter)")/.." && pwd)
-    printf "%bDownloading flutter version %s %b\n" "$GREEN" "$version_to_install" "$NC"
-    git -C "$flutter_root" checkout stable >/dev/null 2>&1
-    git -C "$flutter_root" checkout -b "$version_to_install" "$version_to_install" >/dev/null 2>&1
-    flutter --version
+    command="git -C '$flutter_root' checkout stable >/dev/null 2>&1 && git -C '$flutter_root' checkout -b '$version_to_install' '$version_to_install' >/dev/null 2>&1 && flutter --version >/dev/null 2>&1"
+    run_with_spinner "Downloading flutter version $version_to_install" "Flutter $version_to_install installed successfully" eval "$command"
     git -C "$flutter_root" checkout "$current_branch" >/dev/null 2>&1
-    printf "%b%s%b\n" "$GREEN" "Flutter $version_to_install installed successfully" "$NC"
     printf "%bHINT: Please switch version%b\n" "$YELLOW" "$NC"
 }
 
@@ -103,23 +98,23 @@ downloadFlutterVersion() {
 removeFlutterVersion() {
     version_to_remove=$1
     flutter_root=$(cd "$(dirname "$(which flutter)")/.." && pwd)
-    check=$(git -C "$flutter_root" branch | grep "$branch_to_check")
-
-    # not case
+    check=$(git -C "$flutter_root" branch | grep "$version_to_remove")
+    run_with_spinner "Checking Flutter version $version_to_remove" "" printf ""
+    # Check if the version exists
     if [ -z "$check" ]; then
         logError "Flutter version $version_to_remove is not present in your local system."
         exit 1
     else
+        # Check if the version to remove is the currently active branch
         current_branch=$(git -C "$flutter_root" branch --show-current)
         if [ "$current_branch" == "$version_to_remove" ]; then
             logError "You are currently using this version. Please switch to another version."
             exit 1
         fi
-        printf "%bRemoving flutter version %s %b\n" "$GREEN" "$version_to_remove" "$NC"
-        git -C "$flutter_root" checkout stable >/dev/null 2>&1
-        git -C "$flutter_root" branch -D "$version_to_remove" >/dev/null 2>&1
-        printf "%b%s%b\n" "$GREEN" "Flutter $version_to_remove removed successfully" "$NC"
 
+        # Remove the specified version
+        command="git -C $flutter_root checkout stable >/dev/null 2>&1 && git -C '$flutter_root' branch -D '$version_to_remove' >/dev/null 2>&1"
+        run_with_spinner "Removing Flutter version $version_to_remove" "Flutter $version_to_remove removed successfully" eval "$command"
     fi
 }
 
